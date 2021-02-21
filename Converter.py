51c06fbbd6389a938ab50ca0193afd29e3323b6f
phzsantos <phzsantos@localhost.localdomain>
2021-02-21 15:39:50 -0300

    README; Menu ready

---
+++ Converter.py
@@ -0,0 +1,17 @@
+"""
+This is a little project for starting to learn Python and Logic.
+"""
+
+#First step
+#Create the menu
+
+#Menu
+print('1 - m/s to km/h')
+print('\n2 - km/h to m/s')
+print('\n3 - m/h to km/h')
+print('\n4 - km/h to m/h')
+print('\n5 - m/s to fps')
+print('\n6 - fps to m/s')
+
+
+eWhichOneConversion = input('Select from 1 - 6')
---
+++ README.md
@@ -0,0 +1,7 @@
+#Welcome
+
+###This is one of my first projetct using GitHub
+
+I'm starting in programing so my first program is not complex. It's just a speed converter.
+
+More updates is coming. 
