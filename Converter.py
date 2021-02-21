6625ce2ec66d73cec370c606fd6b32aeb55507a9
phzsantos <phzsantos@localhost.localdomain>
2021-02-21 15:42:13 -0300

    Menu adapted

--- Converter.py
+++ Converter.py
@@ -1,17 +1,39 @@
 """
 This is a little project for starting to learn Python and Logic.
+Start: 21/02/2021
+Name: Paulo Henrique
+Country: Brazil
 """
 
-#First step
-#Create the menu
+#Steps: 
 
-#Menu
+#Step 1:
+#Create the menu [OK]
+
+#Step 2:
+#Create 
+
+#-------------------------------------------------------------------------#
+
+#Import Stage#
+
+import OS
+
+
+#Code Stage#
+
+#Menu#
 print('1 - m/s to km/h')
+
 print('\n2 - km/h to m/s')
+
 print('\n3 - m/h to km/h')
+
 print('\n4 - km/h to m/h')
+
 print('\n5 - m/s to fps')
+
 print('\n6 - fps to m/s')
 
+eWhichOneConversion = input('Select from 1 - 6: ')
 
-eWhichOneConversion = input('Select from 1 - 6')
